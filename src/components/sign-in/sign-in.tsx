8c3d56122c89b2ae7a6ee5e4496f09baae50c922
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2023-07-13 20:04:10 +0300

    feat: welcome-page rendered

---
+++ src/components/sign-in/sign-in.tsx
@@ -0,0 +1,11 @@
+function SignIn(): JSX.Element {
+  return (
+    <li className="header__nav-item">
+      <a className="header__nav-link" href="#">
+        <span className="header__signout">Sign out</span>
+      </a>
+    </li>
+  );
+}
+
+export default SignIn;
